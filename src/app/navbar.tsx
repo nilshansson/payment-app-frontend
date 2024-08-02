5113b3771e6a40a5b29c3517158dd46345263dd8
Otto Bossmann <otto.kostmann@hotmail.com>
2024-08-02 15:20:17 +0200

    car page stuffs
    Co-authored-by: nilshansson <nilsmenghansson@gmail.com>

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Navbar(){
 
 
-  return (<div className="navbar bg-neutral">
+  return (<div className="navbar bg-base-100 ">
           <Link href="/cars">
   <button className="btn btn-ghost btn-xl text-xl">Car shop</button>
 </Link></div>)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,32 +13,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        light: {
-          primary: "#570df8", // Primary color
-          "primary-focus": "#4506cb", // Primary focus
-          "primary-content": "#ffffff", // Foreground for primary
-          secondary: "#f000b8",
-          "secondary-focus": "#bd0091",
-          "secondary-content": "#ffffff",
-          accent: "#37cdbe",
-          "accent-focus": "#2aa79b",
-          "accent-content": "#ffffff",
-          neutral: "#3d4451",
-          "neutral-focus": "#2a2e37",
-          "neutral-content": "#ffffff",
-          "base-100": "#ffffff", // Base color for the main area
-          "base-200": "#f9fafb", // Secondary base color
-          "base-300": "#d1d5db", // Tertiary base color
-          "base-content": "#1f2937", // Text color on base
-          info: "#2094f3",
-          success: "#009485",
-          warning: "#ff9900",
-          error: "#ff5724",
-        },
-      },
-      // You can add more themes here if needed
-    ],
+    themes: ["corporate"],
   },
 };
